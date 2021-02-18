6eb855de3dc4a55292c7433a29012cc8c827f1c4
Starcatch <timurmishiev@gmail.com>
2021-02-18 18:49:17 -0500

    set up a working redux store for users

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render () {
+    return (
+      "Where are my burgers?!" 
+    )
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk  from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-fetch("http://localhost:3000/api/v1/users/1")
-.then(r=>r.json())
-  .then(console.log)
+const users = () => []
+const reducer = combineReducers({
+  users
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
+
+
