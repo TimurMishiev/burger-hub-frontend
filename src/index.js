28cb526d267c9617b42c802686155ecb5a091db7
Starcatch <timurmishiev@gmail.com>
2021-02-18 19:36:55 -0500

    set up separate folder for reducers and actions, and create separate store file to clean index.js file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk  from 'redux-thunk';
+
 import { Provider } from 'react-redux';
+import store from './store.js';
 
-const users = () => []
-const reducer = combineReducers({
-  users
-})
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 
