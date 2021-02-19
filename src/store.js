e864b66955b0de0241db5c54b2b3bc35e5458f6d
Starcatch <timurmishiev@gmail.com>
2021-02-18 19:57:25 -0500

    create currenUser reducer

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import usersReducer from './reducers/users.js';
+import currentUser from '/reducers/currentUser.js'
 import thunk  from 'redux-thunk';
 
 const reducer = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  currentUser //different syntax than line 8
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
